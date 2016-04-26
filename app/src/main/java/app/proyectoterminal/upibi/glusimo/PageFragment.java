20826e42073145784b4de0e5deeba88c2e23deed
GibranValle <gibranvalle@hotmail.com>
2016-04-26 04:22:12 -0500

    Version Beta:
    TabLayout funcionando, se dividio en clase.
    Navigation Drawer lanzada, TODO cambiar de tab

--- app/src/main/java/app/proyectoterminal/upibi/glusimo/PageFragment.java
+++ app/src/main/java/app/proyectoterminal/upibi/glusimo/PageFragment.java
@@ -5,7 +5,6 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 public class PageFragment extends Fragment {
     public static final String ARG_PAGE = "ARG_PAGE";
@@ -29,9 +28,11 @@ public class PageFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        /*
         View view = inflater.inflate(R.layout.fragment_page, container, false);
         TextView textView = (TextView) view;
         textView.setText("Fragment #" + mPage);
-        return view;
+        */
+        return null;
     }
 }
