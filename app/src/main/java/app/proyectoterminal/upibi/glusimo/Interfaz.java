f7513fba8bed2962a4c9efdf33a115ee895cb047
GibranValle <gibranvalle@hotmail.com>
2016-04-26 04:17:14 -0500

    Version Beta:
    TabLayout funcionando, se dividio en clase.
    Navigation Drawer lanzada, TODO cambiar de tab

--- app/src/main/java/app/proyectoterminal/upibi/glusimo/Interfaz.java
+++ app/src/main/java/app/proyectoterminal/upibi/glusimo/Interfaz.java
@@ -1,68 +1,72 @@
 package app.proyectoterminal.upibi.glusimo;
 
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentPagerAdapter;
-import android.support.v4.content.res.ResourcesCompat;
+import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.text.SpannableString;
-import android.text.Spanned;
-import android.text.style.ImageSpan;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-
-import app.proyectoterminal.upibi.glusimo.tabs.SlidingTabLayout;
 
 public class Interfaz extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
-    // CONSTANTES PARA SLIDINGTABVIW
-    final int NUMERO_DE_TABS = 4;
-
-    // VARIABLES PARA SLIDINGTABVIEW
-    private MyFragmentAdapter mMyFragmentAdapter;
-    private ViewPager mViewPager;
-    private Toolbar toolbar;
-    private SlidingTabLayout tabs;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_interfaz);
 
-        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        // Create the adapter that will return a fragment for each of the three
-        // primary sections of the activity.
-        mMyFragmentAdapter = new MyFragmentAdapter(getSupportFragmentManager());
 
-        // Set up the ViewPager with the sections adapter.
-        mViewPager = (ViewPager) findViewById(R.id.container);
-        mViewPager.setAdapter(mMyFragmentAdapter);
+        // Get the ViewPager and set it's PagerAdapter so that it can display items
+        final ViewPager viewPager = (ViewPager) findViewById(R.id.pager);
+        SampleFragmentPagerAdapter pagerAdapter =
+                new SampleFragmentPagerAdapter(getSupportFragmentManager(), Interfaz.this);
+        viewPager.setAdapter(pagerAdapter);
 
-        //TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
-        //tabLayout.setupWithViewPager(mViewPager);
-        tabs = (SlidingTabLayout) findViewById(R.id.tabs);
-        tabs.setCustomTabView(R.layout.custom_tab_view, R.id.tabText);
-        tabs.setViewPager(mViewPager);
+        // Give the TabLayout the ViewPager
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tab_layout);
+        tabLayout.setupWithViewPager(viewPager);
 
+        // Iterate over all tabs and set the custom view
+        for (int i = 0; i < tabLayout.getTabCount(); i++) {
+            TabLayout.Tab tab = tabLayout.getTabAt(i);
+            tab.setCustomView(pagerAdapter.getTabView(i));
+        }
+
+        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+        tabLayout.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
+
+        /* NAVIGATION DRAWER */
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.setDrawerListener(toggle);
         toggle.syncState();
-
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+        /* NAVIGATION DRAWER */
+
     }
 
 
@@ -91,75 +95,5 @@ public class Interfaz extends AppCompatActivity implements NavigationView.OnNavi
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
         return false;
-    }
-
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class MyFragment extends Fragment {
-        /**
-         * The fragment argument representing the section number for this
-         * fragment.
-         */
-        private static final String ARG_SECTION_NUMBER = "section_number";
-
-        public MyFragment() {
-        }
-
-        /**
-         * Returns a new instance of this fragment for the given section
-         * number.
-         */
-        public static MyFragment newInstance(int sectionNumber) {
-            MyFragment fragment = new MyFragment();
-            Bundle args = new Bundle();
-            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
-            fragment.setArguments(args);
-            return fragment;
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_interfaz, container, false);
-            return rootView;
-        }
-    }
-
-    /**
-     * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
-     * one of the sections/tabs/pages.
-     */
-    public class MyFragmentAdapter extends FragmentPagerAdapter {
-        int[] icons = {R.drawable.ic_menu_view, R.drawable.ic_menu_today, R.drawable.ic_menu_search, R.drawable.ic_menu_manage};
-        String[] tabText = getResources().getStringArray(R.array.tabs);
-
-        public MyFragmentAdapter(FragmentManager fm) {
-            super(fm);
-            tabText = getResources().getStringArray(R.array.tabs);
-        }
-
-        @Override
-        public Fragment getItem(int position) {
-            // getItem is called to instantiate the fragment for the given page.
-            // Return a MyFragment (defined as a static inner class below).
-            return MyFragment.newInstance(position + 1);
-        }
-
-        @Override
-        public int getCount() {
-            // Show 3 total pages.
-            return NUMERO_DE_TABS;
-        }
-
-        @Override
-        public CharSequence getPageTitle(int position) {
-            Drawable drawable = ResourcesCompat.getDrawable(getResources(), icons[position], null);
-            drawable.setBounds(0, 0, 36, 36);
-            ImageSpan imageSpan = new ImageSpan(drawable);
-            SpannableString spannableString = new SpannableString(" ");
-            spannableString.setSpan(imageSpan, 0, spannableString.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
-            return tabText[position];
-        }
     }
 }
