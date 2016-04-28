1fa04f9775796ff362763b314a25d59866e28667
GibranValle <gibranvalle@hotmail.com>
2016-04-28 05:31:56 -0500

    Version Beta: Funcionando: - TabLayout - Navigation Drawer - Vibrador - ArcProgressBar - Sonidos - Fragments:     * Bluetooth (comunicando correctamente)     * Acerca de     * Medicion     * Tendencias (parcialmente)
    Por terminar:
    - Canvas
    
    Por implementar:
     - ListView
     - Base de datos
     -Notificacion

--- app/src/main/java/app/proyectoterminal/upibi/glusimo/fragments/Tendencias.java
+++ app/src/main/java/app/proyectoterminal/upibi/glusimo/fragments/Tendencias.java
@@ -1,17 +1,118 @@
 package app.proyectoterminal.upibi.glusimo.fragments;
 
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.PorterDuff;
 import android.os.Bundle;
+import android.os.Vibrator;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.ViewTreeObserver;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 import app.proyectoterminal.upibi.glusimo.R;
 
-public class Tendencias extends Fragment {
+public class Tendencias extends Fragment implements View.OnClickListener {
+
+    TextView consola_tendencias;
+    ImageView espacio;
+    Bitmap bitmap;
+    Canvas canvas;
+    Paint paint;
+    Timer timer;
+    TimerTask timerTask;
+    int alto, ancho;
+    String TAG = "Interfaz";
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_tendencias, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState)
+    {
+        super.onActivityCreated(savedInstanceState);
+
+        consola_tendencias = (TextView) getActivity().findViewById(R.id.consola_tendencias);
+        espacio = (ImageView) getActivity().findViewById(R.id.canvas);
+        espacio.setOnClickListener(this);
+
+        ViewTreeObserver vto = espacio.getViewTreeObserver();
+        vto.addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener()
+        {
+            @Override
+            public void onGlobalLayout()
+            {
+                alto = espacio.getHeight();
+                ancho = espacio.getWidth();
+                //Log.d(TAG, "Height = " + espacio.getHeight() + " Width = " + espacio.getWidth());
+                ViewTreeObserver obs = espacio.getViewTreeObserver();
+                obs.removeGlobalOnLayoutListener(this);
+                Log.i(TAG,"esperando que sirva este metodo alto: "+alto+" ancho: "+ancho);
+                // CREAR CANVAS
+                ///////////// ASIGNACIONES PARA AGREGAR CANVAS /////////////////
+                bitmap = Bitmap.createBitmap(ancho, alto, Bitmap.Config.ARGB_8888);
+                espacio.setImageBitmap(bitmap);
+                canvas = new Canvas(bitmap);
+                paint = new Paint(Paint.FILTER_BITMAP_FLAG);
+                empezarCanvas();
+                ///////////// ASIGNACIONES PARA AGREGAR CANVAS ////////////////
+            }
+        });
+    }
+
+    @Override
+    public void onClick(View v)
+    {
+        vibrar(100);
+    }
+
+    public void empezarCanvas()
+    {
+        Log.d(TAG, "limpiando canvas");
+        canvas.drawColor(Color.TRANSPARENT, PorterDuff.Mode.CLEAR);
+        paint.setColor(0xFF960000);
+        paint.setStrokeWidth(1);
+        // LINEAS VERTICALES
+        canvas.drawLine(1 * ancho / 10, 0, 1 * ancho / 10, alto, paint);
+        canvas.drawLine(2 * ancho / 10, 0, 2 * ancho / 10, alto, paint);
+        canvas.drawLine(3 * ancho / 10, 0, 3 * ancho / 10, alto, paint);
+        canvas.drawLine(4 * ancho / 10, 0, 4 * ancho / 10, alto, paint);
+        canvas.drawLine(5 * ancho / 10, 0, 5 * ancho / 10, alto, paint);
+        canvas.drawLine(6 * ancho / 10, 0, 6 * ancho / 10, alto, paint);
+        canvas.drawLine(7 * ancho / 10, 0, 7 * ancho / 10, alto, paint);
+        canvas.drawLine(8 * ancho / 10, 0, 8 * ancho / 10, alto, paint);
+        canvas.drawLine(9 * ancho / 10, 0, 9 * ancho / 10, alto, paint);
+        // LINEAS HORIZONTALES
+        canvas.drawLine(0, 1 * alto / 10 ,ancho, 1 * alto / 10, paint);
+        canvas.drawLine(0, 2 * alto / 10 ,ancho, 2 * alto / 10, paint);
+        canvas.drawLine(0, 3 * alto / 10 ,ancho, 3 * alto / 10, paint);
+        canvas.drawLine(0, 4 * alto / 10 ,ancho, 4 * alto / 10, paint);
+        canvas.drawLine(0, 5 * alto / 10 ,ancho, 5 * alto / 10, paint);
+        canvas.drawLine(0, 6 * alto / 10 ,ancho, 6 * alto / 10, paint);
+        canvas.drawLine(0, 7 * alto / 10 ,ancho, 7 * alto / 10, paint);
+        canvas.drawLine(0, 8 * alto / 10 ,ancho, 8 * alto / 10, paint);
+        canvas.drawLine(0, 9 * alto / 10 ,ancho, 9 * alto / 10, paint);
+        // EDITAR COLOR PARA GRAFICA
+        paint.setColor(0xffffffff);
+        paint.setStrokeWidth(3);
+    }
+
+    private void vibrar(int ms)
+    {
+        Vibrator vibrador = (Vibrator) getActivity().getSystemService(Context.VIBRATOR_SERVICE);
+        vibrador.vibrate(ms);
     }
 }
