03fcc4ee809ab06ae286abbaf19254590253577c
GibranValle <gibranvalle@hotmail.com>
2016-04-29 07:47:29 -0500

    Version Beta: Funcionando: - TabLayout - Navigation Drawer - Vibrador - ArcProgressBar - Sonidos - Fragments:     * Bluetooth (comunicando correctamente)     * Acerca de     * Medicion     * Tendencias (parcialmente)
    Por terminar:
    - Canvas
    
    Por implementar:
     - ListView
     - Base de datos
     -Notificacion

--- app/src/main/java/app/proyectoterminal/upibi/glusimo/fragments/Tendencias.java
+++ app/src/main/java/app/proyectoterminal/upibi/glusimo/fragments/Tendencias.java
@@ -31,7 +31,7 @@ import app.proyectoterminal.upibi.glusimo.R;
 public class Tendencias extends Fragment implements View.OnClickListener {
 
     TextView consola_tendencias;
-    ImageView espacio;
+    ImageView espacio,ejeY;
     Timer timer;
     TimerTask timerTask;
     EventBus bus = EventBus.getDefault();
@@ -41,11 +41,18 @@ public class Tendencias extends Fragment implements View.OnClickListener {
 
     int puntos = 0;
     float gain = 2.5f;
+
     public Bitmap bitmap;
     public Canvas canvas;
     public Paint paint;
-
     int alto, ancho;
+
+    public Bitmap bitmap2;
+    public Canvas canvas2;
+    public Paint paint2;
+    int alto2, ancho2;
+
+
     String TAG = "Interfaz";
     int conteo;
     final Handler handler = new Handler();  // VARIABLE PARA TIMER
@@ -76,6 +83,7 @@ public class Tendencias extends Fragment implements View.OnClickListener {
         super.onActivityCreated(savedInstanceState);
 
         consola_tendencias = (TextView) getActivity().findViewById(R.id.consola_tendencias);
+        ejeY = (ImageView) getActivity().findViewById(R.id.eje_concentracion);
         espacio = (ImageView) getActivity().findViewById(R.id.canvas);
         espacio.setOnClickListener(this);
 
@@ -88,22 +96,51 @@ public class Tendencias extends Fragment implements View.OnClickListener {
             @Override
             public void onGlobalLayout()
             {
+                // CONSIGUE LOS DATOS DE ANCHO Y ALTO HASTA ESTE PUNTO
                 alto = espacio.getHeight();
                 ancho = espacio.getWidth();
                 pss = (float) ancho/tiempo.length;
                 Log.i(TAG,"paso calculado: "+pss);
+
+                //
+                alto2 = ejeY.getHeight();
+                ancho2 = ejeY.getWidth();
 
                 //Log.d(TAG, "Height = " + espacio.getHeight() + " Width = " + espacio.getWidth());
                 ViewTreeObserver obs = espacio.getViewTreeObserver();
                 obs.removeGlobalOnLayoutListener(this);
                 Log.i(TAG,"esperando que sirva este metodo alto: "+alto+" ancho: "+ancho);
 
+                /*
                 bitmap = Bitmap.createBitmap(ancho, alto, Bitmap.Config.ARGB_8888);
                 espacio.setImageBitmap(bitmap);
                 canvas = new Canvas(bitmap);
                 paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+                */
 
-                empezarCanvas();
+                bitmap2 = Bitmap.createBitmap(ancho2, alto2, Bitmap.Config.ARGB_8888);
+                ejeY.setImageBitmap(bitmap2);
+                canvas2 = new Canvas(bitmap2);
+                paint2 = new Paint(Paint.ANTI_ALIAS_FLAG);
+                paint2.setColor(Color.WHITE);
+                canvas2.drawText("Concentracion [mg/mL]",20,20,paint2);
+                canvas2.translate(ancho2/2, alto2/2);
+                canvas2.drawText("Concentracion [mg/mL]",20,20,paint2);
+                canvas2.translate(ancho2, alto2);
+                canvas2.drawText("Concentracion [mg/mL]",20,20,paint2);
+                /*
+                canvas2.translate(-ancho2/2, -alto2/2);
+                canvas2.drawText("Concentracion [mg/mL]",0,0,paint2);
+                canvas2.rotate(-10);
+                canvas2.drawText("Concentracion [mg/mL]",0,0,paint2);
+                canvas2.drawText("Concentracion [mg/mL]",ancho2,alto2,paint2);
+                canvas2.rotate(-10);
+                canvas2.drawText("Concentracion [mg/mL]",0,0,paint2);
+                canvas2.drawText("Concentracion [mg/mL]",ancho2,alto2,paint2);
+                paint2.setStyle(Paint.Style.FILL);
+                */
+                //empezarCanvas();
+
                 // calcular el paso en X
                 ///////////// ASIGNACIONES PARA AGREGAR CANVAS ////////////////
             }
